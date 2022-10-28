84978218d0678e2990878675fc428cafe6b4ffbc
Vishal Poddar <vishalpoddar@Vishals-MacBook-Air.local>
2022-10-28 21:17:11 +0530

    initial Commit

---
+++ src/main/java/com/vishal/wacaj/WacajApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.wacaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WacajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WacajApplication.class, args);
+	}
+
+}
