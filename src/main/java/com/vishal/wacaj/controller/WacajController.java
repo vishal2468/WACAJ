cbca1d3870cdaaf9d67192ff7d4ebd5ba8c01b63
Vishal Poddar <vishalpoddar@Vishals-MacBook-Air.local>
2022-10-31 13:32:10 +0530

    added webhook backbone for all message types

--- src/main/java/com/vishal/wacaj/controller/WacajController.java
+++ src/main/java/com/vishal/wacaj/controller/WacajController.java
@@ -6,16 +6,59 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vishal.wacaj.model.webhook.Messages;
 import com.vishal.wacaj.model.webhook.Payload;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @RestController
+@Slf4j
 public class WacajController {
     @PostMapping("/webhooks")
-    public Payload hook(@RequestBody Payload payload) {
+    public Messages hook(@RequestBody Payload payload) {
         // todo
         // handle all the cases here for different types of webhook body
-        
-        return payload;
+        String changedField=payload.getEntry().get(0).getChanges().get(0).getField();
+        if(changedField.equals("messages")){
+            String messageType=payload.getEntry().get(0).getChanges().get(0).getValue().getMessages().get(0).getType();
+            if(messageType.equals("text")){
+                log.info("Message type is : text");
+                // take apt actions
+                return payload.getEntry().get(0).getChanges().get(0).getValue().getMessages().get(0);
+            }
+            else if(messageType.equals("button")){
+                log.info("Message type is : button");
+            }
+            else if(messageType.equals("document")){
+                log.info("Message type is : document");
+            }
+            else if(messageType.equals("image")){
+                log.info("Message type is : image");
+            }
+            else if(messageType.equals("interactive")){
+                log.info("Message type is : interactive");
+            }
+            else if(messageType.equals("order")){
+                log.info("Message type is : order");
+            }
+            else if(messageType.equals("sticker")){
+                log.info("Message type is : sticker");
+            }
+            else if(messageType.equals("video")){
+                log.info("Message type is : video");
+            }
+            else if(messageType.equals("system")){
+                log.info("Message type is : system");
+            }
+            else if(messageType.equals("unknown")){
+                log.info("Message type is : unknown");
+            }
+            else{
+                log.info("Message is not of any predefined types");
+            }
+        }
+        return payload.getEntry().get(0).getChanges().get(0).getValue().getMessages().get(0);
     }
 
     @GetMapping("/webhooks")
