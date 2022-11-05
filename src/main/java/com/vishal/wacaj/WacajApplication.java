eaa273d3fd8aa2fffd4f063b476703dadfb51056
vishal2468 <vishalpoddar2468@gmail.com>
2022-11-05 21:22:32 +0530

    added mongo db atlas uri

--- src/main/java/com/vishal/wacaj/WacajApplication.java
+++ src/main/java/com/vishal/wacaj/WacajApplication.java
@@ -2,8 +2,10 @@ package com.vishal.wacaj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class WacajApplication {
 
 	public static void main(String[] args) {
